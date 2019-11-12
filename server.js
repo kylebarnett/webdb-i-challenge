297f78589435c9e2c7756976d7841d6488852f8d
Kyle Barnett <kbarnett23@gmail.com>
2019-11-12 15:41:06 -0700

    PUT method working

--- server.js
+++ server.js
@@ -6,4 +6,55 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  db('accounts')
+    .then(account => {
+      console.log(account)
+      res.status(200).json(account)
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Cannot get accounts.' })
+    })
+})
+
+server.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('accounts').where({ id })
+    .then(account => {
+      if (account) {
+        res.status(200).json(account)
+      } else {
+        res.status(404).json({ message: 'ID not found.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error retrieving ID.' })
+    })
+})
+
+server.post('/', (req, res) => {
+  const { body } = req;
+  db('accounts').insert(body)
+    .then(account => {
+      res.status(201).json(account)
+    })
+})
+
+server.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const bodyInfo = req.body;
+
+  db('accounts').where({ id }).update(bodyInfo)
+    .then(account => {
+      if (account) {
+        res.status(201).json({ updated: account })
+      } else {
+        res.status(404).json({ message: 'Invalid account ID.' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error updating account.' })
+    })
+})
+
 module.exports = server;

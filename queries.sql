5eeccb64827c40d02d1275537ad54e83e94b9a7b
Kyle Barnett <kbarnett23@gmail.com>
2019-11-12 12:23:16 -0700

    stretch for sql done

--- queries.sql
+++ queries.sql
@@ -23,5 +23,7 @@ values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle
 update customers set postalcode='11122' where contactname='Bilbo Baggins'
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
+SELECT DISTINCT City FROM Customers;
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+select * from Suppliers where length(SupplierName) > 20
